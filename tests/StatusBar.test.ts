64a0c59d72c032f180e1b974036262e209b55844
goooice <devel0per1991@outlook.com>
2022-06-21 09:58:40 +0800

    test: status bar rename join to register

--- tests/StatusBar.test.ts
+++ tests/StatusBar.test.ts
@@ -6,10 +6,10 @@ describe('StatusBar', () => {
     expect(new StatusBar()).toBeTruthy();
   });
 
-  it('add 3 status bar items', () => {
+  it('register 3 status bar items', () => {
     // const createStatusBarItem = vscode.window.createStatusBarItem() as unknown as jest.Mock<{}>;
     const statusBar = new StatusBar();
-    statusBar.jion();
+    statusBar.register();
     expect(vscode.window.createStatusBarItem).toBeCalledTimes(3);
   });
 });
