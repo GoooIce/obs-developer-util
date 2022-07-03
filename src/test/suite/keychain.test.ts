f1183314da7d094a81bf0c606e263b419364f6a8
goooice <devel0per1991@outlook.com>
2022-07-03 09:55:12 +0800

    fix(test): can not setPassword without test function chore: use pecock change vscode bars color

--- src/test/suite/keychain.test.ts
+++ src/test/suite/keychain.test.ts
@@ -4,14 +4,9 @@ import { keychain } from '../../keychain';
 import * as assert from 'assert';
 
 suite('Keychain Test Suite', async () => {
-  await keychain?.setPassword('test', 'test', 'password');
-  test('get it', async () => {
+  test('set&get it', async () => {
+    await keychain?.setPassword('test', 'test', 'password');
     const pw = await keychain?.getPassword('test', 'test');
     assert.equal('password', pw);
-  });
-  test('del it', async () => {
-    await keychain?.deletePassword('test', 'test');
-    const pw = keychain?.getPassword('test', 'test');
-    assert.equal('', pw);
   });
 });
