c4213857fbeccb784b1902861af511eee9fc0227
王雪 <broken.ice1314@gmail.com>
2022-07-31 12:13:51 +0800

    test: add genIdentifyMessage unit

--- tests/authentication.test.ts
+++ tests/authentication.test.ts
@@ -1,37 +1,56 @@
 jest.unmock('../src/obs-websocket/util');
-import { IncomingMessage, WebSocketOpCode } from '../src/obs-websocket/types';
-import { genAuthString, needAuth } from '../src/obs-websocket/util';
+import {
+  EventSubscription,
+  IncomingMessage,
+  OutgoingMessage,
+  WebSocketOpCode,
+} from '../src/obs-websocket/types';
+import { genAuthString, needAuth, genIdentifyMessage } from '../src/obs-websocket/util';
 
 describe('authentication', () => {
-  it('server need auth', () => {
-    const msg: IncomingMessage<WebSocketOpCode.Hello> = {
-      op: WebSocketOpCode.Hello,
-      d: {
-        obsWebSocketVersion: '5.0.0',
-        rpcVersion: 1,
-        authentication: {
-          salt: 'lM1GncleQOaCu9lT1yeUZhFYnqhsLLP1G5lAGo3ixaI=',
-          challenge: '+IxH4CnCiqpX1rM9scsNynZzbOe4KhDeYcTNS3PDaeY=',
-        },
+  const salt = 'lM1GncleQOaCu9lT1yeUZhFYnqhsLLP1G5lAGo3ixaI=';
+  const challenge = '+IxH4CnCiqpX1rM9scsNynZzbOe4KhDeYcTNS3PDaeY=';
+  const password = 'supersecretpassword';
+  const rightAuthString = '1Ct943GAT+6YQUUX47Ia/ncufilbe6+oD6lY+5kaCu4=';
+  const needAuthMsg: IncomingMessage<WebSocketOpCode.Hello> = {
+    op: WebSocketOpCode.Hello,
+    d: {
+      obsWebSocketVersion: '5.0.0',
+      rpcVersion: 1,
+      authentication: {
+        salt: 'lM1GncleQOaCu9lT1yeUZhFYnqhsLLP1G5lAGo3ixaI=',
+        challenge: '+IxH4CnCiqpX1rM9scsNynZzbOe4KhDeYcTNS3PDaeY=',
       },
-    };
-    expect(needAuth(msg)).toBe(true);
+    },
+  };
+  const doNotNeedAuthMsg: IncomingMessage<WebSocketOpCode.Hello> = {
+    op: WebSocketOpCode.Hello,
+    d: {
+      obsWebSocketVersion: '5.0.0',
+      rpcVersion: 1,
+    },
+  };
+  it('server need auth', () => {
+    expect(needAuth(needAuthMsg)).toBe(true);
   });
   it('server do not need auth', () => {
-    const msg: IncomingMessage<WebSocketOpCode.Hello> = {
-      op: WebSocketOpCode.Hello,
-      d: {
-        obsWebSocketVersion: '5.0.0',
-        rpcVersion: 1,
-      },
-    };
-    expect(needAuth(msg)).toBe(false);
+    expect(needAuth(doNotNeedAuthMsg)).toBe(false);
   });
   it('gen obs auth string', () => {
-    const salt = 'lM1GncleQOaCu9lT1yeUZhFYnqhsLLP1G5lAGo3ixaI=';
-    const challenge = '+IxH4CnCiqpX1rM9scsNynZzbOe4KhDeYcTNS3PDaeY=';
-    const password = 'supersecretpassword';
-    const authString = genAuthString({ salt, challenge }, password);
-    expect(authString).toBe('1Ct943GAT+6YQUUX47Ia/ncufilbe6+oD6lY+5kaCu4=');
+    expect(genAuthString({ salt, challenge }, password)).toBe(rightAuthString);
+  });
+  it('re back hello op by identify', () => {
+    const outMsg: OutgoingMessage<WebSocketOpCode.Identify> = genIdentifyMessage(
+      needAuthMsg,
+      EventSubscription.None
+    );
+    expect(outMsg).toBe({
+      op: 1,
+      d: {
+        rpcVersion: 1,
+        authentication: rightAuthString,
+        eventSubscriptions: 0,
+      },
+    });
   });
 });
