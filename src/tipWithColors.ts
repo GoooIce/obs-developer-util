2b2e7def1c8e40b0e273854ffedab1399a3ac64d
王雪 <broken.ice1314@gmail.com>
2022-08-10 09:12:06 +0800

    chore: learn timer of rxjs

--- src/tipWithColors.ts
+++ src/tipWithColors.ts
@@ -1,36 +1,19 @@
 import * as vscode from 'vscode';
-import { Observable, Subscriber } from 'rxjs';
+import { interval, take, finalize, map } from 'rxjs';
 
 export function tipWithColors(then_callback?: () => {}) {
   const peacock_config = vscode.workspace.getConfiguration('peacock');
-  let i = 1;
-  const color$ = new Observable((subscriber: Subscriber<string>) => {
-    setInterval(() => {
-      if (4 <= i) subscriber.complete();
-      if (1 == i) {
-        subscriber.next('#dd0531');
-      }
-      if (2 == i) {
-        subscriber.next('#f9e64f');
-      }
-      if (3 == i) {
-        subscriber.next('#007fff');
-      }
-      i++;
-    }, 1000);
-  });
-
-  color$.subscribe({
-    next(color) {
-      peacock_config.update('color', color);
-    },
-    complete() {
+  const colorArr = ['#dd0531', '#f9e64f', '#007fff'];
+  const colorSource$ = interval(1000).pipe(
+    take(3),
+    map((value) => {
+      peacock_config.update('color', colorArr[value]);
+    }),
+    finalize(() => {
       vscode.commands.executeCommand('peacock.resetWorkspaceColors');
-      // vscode.commands.executeCommand(recordCommandId);
-      if (then_callback) then_callback();
-    },
-    error(err) {
-      vscode.window.showErrorMessage(err);
-    },
-  });
+      if (then_callback) return then_callback;
+    })
+  );
+
+  colorSource$.subscribe();
 }
