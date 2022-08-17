b625cf52d2bbbb5b8a5d3bdbaa59e2f26c02ec3e
王雪 <broken.ice1314@gmail.com>
2022-08-17 12:37:35 +0800

    react

--- src/panels/BasePanels.ts
+++ src/panels/BasePanels.ts
@@ -8,13 +8,20 @@ export class BasePanel {
   public static currentPanel: BasePanel | undefined;
   private readonly _panel: vscode.WebviewPanel;
   private _disposables: vscode.Disposable[] = [];
+  // private _extensionUri: vscode.Uri;
 
   private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    // this._extensionUri = extensionUri;
     this._panel = panel;
     this._panel.webview.html = this._getWebviewContent(this._panel.webview, extensionUri);
 
     this._panel.onDidDispose(this.dispose, null, this._disposables);
     this._setWebviewMessageListener(this._panel.webview);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public postMessage(msg: any) {
+    this._panel.webview.postMessage(msg);
   }
 
   private _setWebviewMessageListener(webview: vscode.Webview) {
@@ -27,6 +34,10 @@ export class BasePanel {
         switch (command) {
           case 'hello':
             vscode.window.showInformationMessage(text);
+            // // eslint-disable-next-line no-case-declarations
+            // const clapperboard_1 = this._getUri(webview, ['lottie', 'clapperboard-1.json']);
+
+            // webview.postMessage({ uri: clapperboard_1 });
             return;
         }
       },
@@ -36,45 +47,52 @@ export class BasePanel {
   }
 
   private _getWebviewContent(webview: vscode.Webview, extensionUri: vscode.Uri): string {
-    const toolkitUri = getUri(webview, extensionUri, [
-      'node_modules',
-      '@vscode',
-      'webview-ui-toolkit',
-      'dist',
-      'toolkit.js',
-    ]);
-    const mainUri = getUri(webview, extensionUri, ['src', 'webview-ui', 'main.js']);
-    const lottieUri = getUri(webview, extensionUri, [
-      'node_modules',
-      '@lottiefiles',
-      'lottie-player',
-      'dist',
-      'lottie-player.js',
-    ]);
-    const clapperboard_1 = getUri(webview, extensionUri, ['lottie', 'clapperboard-1.json']);
+    // const rxjsUri = this._getUri(webview, [
+    //   'node_modules',
+    //   'rxjs',
+    //   'dist',
+    //   'bundles',
+    //   'rxjs.umd.js',
+    // ]);
+    // <script type="module" src="${rxjsUri}"></script>;
+    // const toolkitUri = this._getUri(webview, [
+    //   'node_modules',
+    //   '@vscode',
+    //   'webview-ui-toolkit',
+    //   'dist',
+    //   'toolkit.js',
+    // ]);
+    // const mainUri = this._getUri(webview, ['src', 'webview-ui', 'main.js']);
+    // const lottieUri = this._getUri(webview, [
+    //   'node_modules',
+    //   '@lottiefiles',
+    //   'lottie-player',
+    //   'dist',
+    //   'lottie-player.js',
+    // ]);
+    // const clapperboard_1 = this._getUri(webview, ['lottie', 'clapperboard-1.json']);
+    // The CSS file from the React build output
+    const stylesUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'assets', 'index.css']);
+    // The JS file from the React build output
+    const scriptUri = getUri(webview, extensionUri, ['webview-ui', 'build', 'assets', 'index.js']);
+
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
     return /*html*/ `
       <!DOCTYPE html>
       <html lang="en">
         <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width,initial-scale=1.0">
-          <script type="module" src="${toolkitUri}"></script>
-          <script type="module" src="${lottieUri}"></script>
-          <script type="module" src="${mainUri}"></script>
-          <title>Hello World!</title>
+          <meta charset="UTF-8" />
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="default-src 'none'; img-src *; script-src *; style-src *;"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <title>Hello World</title>
         </head>
         <body>
-          <h1>Hello World!</h1>
-          <vscode-button id="howdy">Howdy!</vscode-button>
-          <lottie-player
-            loop
-            controls
-            autoplay
-            mode="normal"
-            src="${clapperboard_1}"
-            style="width: 320px"
-          >
-          </lottie-player>
+          <div id="root"></div>
+          <script type="module" src="${scriptUri}"></script>
         </body>
       </html>
     `;
