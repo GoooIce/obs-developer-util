94992b4b76f16adf09e80a50e6c137b2c03c8345
王雪 <broken.ice1314@gmail.com>
2022-08-24 17:35:21 +0800

    chore: add dev mock lib jest-websocket-mock

---
+++ tests/src/obs-websocket/index.test.ts
@@ -0,0 +1,21 @@
+import * as index from '../../src/obs-websocket';
+import { TestScheduler } from 'rxjs/testing';
+
+describe('index', () => {
+  let testScheduler: TestScheduler;
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('should export static websocket subject creator functions', () => {
+    expect(index.webSocket).toBeDefined();
+  });
+
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip('auth', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    testScheduler.run(({ expectObservable, expectSubscriptions }) => {});
+  });
+});
