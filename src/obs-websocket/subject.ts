c56e3496667557a54285457aefbcee9815d89cb8
王雪 <broken.ice1314@gmail.com>
2022-08-27 12:07:46 +0800

    refactor: obs websocket subject class

--- src/obs-websocket/subject.ts
+++ src/obs-websocket/subject.ts
@@ -1,5 +1,5 @@
 import { Subject } from 'rxjs';
-import { WebSocketSubject, WebSocketSubjectConfig } from 'rxjs/webSocket';
+import { WebSocketSubject, WebSocketSubjectConfig, webSocket } from 'rxjs/webSocket';
 import { Message } from './types';
 
 interface OnWebSocketLife {
@@ -7,9 +7,30 @@ interface OnWebSocketLife {
   onClose$: Subject<void>;
 }
 
+type OBSWebSocketSubject = WebSocketSubject<Message>;
+type OBSWebSocketSubjectConfig = WebSocketSubjectConfig<Message>;
+
 export class OBSSubject implements OnWebSocketLife {
-  onOpen$: Subject<void> = new Subject();
-  onClose$: Subject<void> = new Subject();
-  private _obs_ws_subject$: WebSocketSubject<Message>;
-  private _config: WebSocketSubjectConfig<Message>;
+  private static obs_subject: OBSSubject;
+  onOpen$: Subject<void>;
+  onClose$: Subject<void>;
+  private readonly _ws_subject$: OBSWebSocketSubject;
+  // private _config: WebSocketSubjectConfig<Message>;
+
+  private constructor(config: OBSWebSocketSubjectConfig) {
+    this.onClose$ = new Subject();
+    this.onOpen$ = new Subject();
+    this._ws_subject$ = webSocket(config);
+  }
+
+  public static getSubject(config?: OBSWebSocketSubjectConfig) {
+    if (this.obs_subject) return this.obs_subject;
+    if (undefined === config) {
+      config = {
+        url: 'localhost:4455',
+      };
+    }
+    this.obs_subject = new this(config);
+    return this.obs_subject;
+  }
 }
