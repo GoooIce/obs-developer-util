2d2ce1d76a0b1ebdb6b06f35e6f3d2a7cf6b9d50
王雪 <broken.ice1314@gmail.com>
2022-08-30 18:09:36 +0800

    refactor: obs-websocket subject auth flow

--- src/obs-websocket/subject.ts
+++ src/obs-websocket/subject.ts
@@ -1,6 +1,9 @@
-import { Subject } from 'rxjs';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { filter, map, Observable, Subject, tap } from 'rxjs';
 import { WebSocketSubject, WebSocketSubjectConfig, webSocket } from 'rxjs/webSocket';
-import { Message } from './types';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { EventSubscription, Message, RequestMessage, WebSocketOpCode } from './types';
+import { needAuth, genIdentifyMessage } from './util';
 
 interface OnWebSocketLife {
   onOpen$: Subject<void>;
@@ -12,22 +15,89 @@ type OBSWebSocketSubjectConfig = WebSocketSubjectConfig<Message>;
 
 export class OBSSubject implements OnWebSocketLife {
   private static obs_subject: OBSSubject;
+  onAuth$: Subject<void>;
   onOpen$: Subject<void>;
   onClose$: Subject<void>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onError$: Subject<any>;
+  onComplete$: Subject<void>;
+  password$: Subject<string>;
+  identified: boolean;
   private readonly _ws_subject$: OBSWebSocketSubject;
   // private _config: WebSocketSubjectConfig<Message>;
+
+  public next(msg: Message) {
+    this._ws_subject$.next(msg);
+  }
 
   private constructor(config: OBSWebSocketSubjectConfig) {
     this.onClose$ = new Subject();
     this.onOpen$ = new Subject();
+    this.onAuth$ = new Subject();
+    this.onError$ = new Subject();
+    this.onComplete$ = new Subject();
+    this.password$ = new Subject();
+    this.identified = false;
     this._ws_subject$ = webSocket(config);
+
+    const onAuth$ = this.onAuth$;
+    const password$ = this.password$;
+
+    this._ws_subject$
+      .pipe(
+        filter((msg) => msg.op === WebSocketOpCode.Identified),
+        tap(() => {
+          this.identified = true;
+        })
+      )
+      .subscribe();
+
+    const identify$ = this._ws_subject$.pipe(
+      filter((msg) => msg.op === WebSocketOpCode.Hello)
+    ) as Observable<Message<WebSocketOpCode.Hello>>;
+
+    identify$.subscribe();
+    // need auth
+    identify$.pipe(filter((msg) => needAuth(msg))).subscribe({
+      next(msg) {
+        onAuth$.next();
+        password$.subscribe({
+          next(password) {
+            OBSSubject.obs_subject._ws_subject$.next(
+              genIdentifyMessage(msg, EventSubscription.All, password)
+            );
+          },
+        });
+      },
+    });
+
+    // dont need auth
+    identify$.pipe(filter((msg) => !needAuth(msg))).subscribe({
+      next(msg) {
+        OBSSubject.obs_subject._ws_subject$.next(
+          genIdentifyMessage(msg, EventSubscription.All, '')
+        );
+      },
+    });
+
+    this._ws_subject$.subscribe({
+      next: (msg: Message) => {
+        if (WebSocketOpCode.RequestResponse === msg.op) {
+          console.log(`${msg.d.requestType} : result : ${msg.d.requestStatus.result}`);
+        }
+      }, // Called whenever there is a message from the server.
+      error: (err) => this.onError$.next(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => {
+        this.onComplete$.next();
+      },
+    });
   }
 
   public static getSubject(config?: OBSWebSocketSubjectConfig) {
     if (this.obs_subject) return this.obs_subject;
     if (undefined === config) {
       config = {
-        url: 'localhost:4455',
+        url: 'ws://localhost:4455',
       };
     }
     this.obs_subject = new this(config);
--- tests/src/obs-websocket/subject.test.ts
+++ tests/src/obs-websocket/subject.test.ts
@@ -1,20 +1,67 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 jest.unmock('../../../src/obs-websocket/subject');
+jest.unmock('../../../src/obs-websocket/util');
 jest.unmock('jest-websocket-mock');
 import WS from 'jest-websocket-mock';
 import { OBSSubject } from '../../../src/obs-websocket/subject';
+import { Message, WebSocketOpCode } from '../../../src/obs-websocket/types';
 // import { TestScheduler } from 'rxjs/testing';
 
 describe('subject', () => {
   let ws: WS;
+  const password = 'supersecretpassword';
+  const helloOpMsg: Message<WebSocketOpCode.Hello> = {
+    op: WebSocketOpCode.Hello,
+    d: { obsWebSocketVersion: '5.0.0', rpcVersion: 1 },
+  };
+  const helloOpMsgWithAuth: Message<WebSocketOpCode.Hello> = {
+    op: WebSocketOpCode.Hello,
+    d: {
+      obsWebSocketVersion: '5.0.0',
+      rpcVersion: 1,
+      authentication: {
+        challenge: '+IxH4CnCiqpX1rM9scsNynZzbOe4KhDeYcTNS3PDaeY=',
+        salt: 'lM1GncleQOaCu9lT1yeUZhFYnqhsLLP1G5lAGo3ixaI=',
+      },
+    },
+  };
+  const identifyOpMsg: Message<WebSocketOpCode.Identify> = {
+    op: WebSocketOpCode.Identify,
+    d: { rpcVersion: 1 },
+  };
+  const identifyOpMsgWithAuth: Message<WebSocketOpCode.Identify> = {
+    op: WebSocketOpCode.Identify,
+    d: { rpcVersion: 1, authentication: '1Ct943GAT+6YQUUX47Ia/ncufilbe6+oD6lY+5kaCu4=' },
+  };
+
+  const identifiedOpMsg: Message<WebSocketOpCode.Identified> = {
+    op: WebSocketOpCode.Identified,
+    d: {
+      negotiatedRpcVersion: 1,
+    },
+  };
   // let testScheduler: TestScheduler;
   beforeEach(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    ws = new WS('ws://localhost:4455');
+    ws = new WS('ws://localhost:4455', { jsonProtocol: true });
     // testScheduler = new TestScheduler((actual, expected) => {
     //   expect(actual).toEqual(expected);
     // });
   });
   afterEach(() => WS.clean());
+
+  it('jest-websocket-mock work', () => {
+    const client = new WebSocket('ws://localhost:4455');
+    client.send('{"result":true, "count":42}');
+
+    expect(ws).toReceiveMessage({ result: true, count: 42 });
+    // expect(ws).toHaveReceivedMessages(['hello']);
+  });
+
+  it('obssubject next send message', () => {
+    const obs = OBSSubject.getSubject();
+    obs.next(identifyOpMsg);
+    expect(ws).toReceiveMessage(identifiedOpMsg);
+  });
 
   it('sigleton', () => {
     // testScheduler.run(({ expectObservable, expectSubscriptions }) => {});
@@ -36,5 +83,25 @@ describe('subject', () => {
     obs.onOpen$.next();
 
     expect(foo).toBeFalsy();
+  });
+
+  it('onAuth', () => {
+    const obs = OBSSubject.getSubject();
+    obs.onAuth$.subscribe({
+      next: (value) => {
+        obs.password$.next(password);
+      },
+    });
+
+    // ws.connected;
+    ws.send(helloOpMsgWithAuth);
+    expect(obs.identified).toBeFalsy();
+    ws.send({
+      op: WebSocketOpCode.Identified,
+      d: {
+        negotiatedRpcVersion: 1,
+      },
+    });
+    expect(obs.identified).toBeTruthy();
   });
 });
