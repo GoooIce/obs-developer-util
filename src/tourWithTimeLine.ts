cfe58186cd44b11ff7c556a63bb8f154c9a31d96
王雪 <broken.ice1314@gmail.com>
2022-09-30 10:28:52 +0800

    feat: record video object with clip

--- src/tourWithTimeLine.ts
+++ src/tourWithTimeLine.ts
@@ -1,7 +1,21 @@
-// import { writeFile } from 'fs';
+import { writeFileSync, existsSync } from 'fs';
 import { map } from 'rxjs';
 import * as vscode from 'vscode';
+import { stopRecordCommandId } from './enum';
 import { OBSSubject } from './obs-websocket/subject';
+
+const _json_ld_template = {
+  '@context': 'https://schema.org/',
+  '@type': 'VideoObject',
+  name: 'codetour video',
+  hasPart: [
+    {
+      '@type': 'Clip',
+      name: '前言',
+      offset: '0',
+    },
+  ],
+};
 
 export function onDidToursRecord(_context: vscode.ExtensionContext) {
   // Check if the end-user has the CodeTour extension installed.
@@ -9,21 +23,69 @@ export function onDidToursRecord(_context: vscode.ExtensionContext) {
   if (codeTourExtension) {
     // Grab the extension API.
     const codeTourApi = codeTourExtension.exports;
+    // 如果没有在录制 并且 .tour文件夹存在
+    if (vscode.workspace.workspaceFolders) {
+      const tourFolder = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, '.tours');
 
-    // Use the API object as needed
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    codeTourApi.onDidStartTour(([tour, stepNumber]: [{ title: string }, number]) => {
-      console.log('Tour started: ', tour.title);
-      const obs = OBSSubject.getSubject();
-      obs
-        .GetRecordStatus()
-        .pipe(map((res) => [res.responseData.outputDuration, res.responseData.outputTimecode]))
-        .subscribe();
-      if (vscode.workspace.workspaceFolders) console.log(vscode.workspace.workspaceFolders);
-    });
+      if (existsSync(tourFolder.fsPath)) {
+        // changeObsOutputConfig(tourFolder.uri.fsPath);
+        ganVideoObject(_context, codeTourApi, vscode.workspace.workspaceFolders[0].uri.fsPath);
+      }
+    }
 
-    codeTourApi.onDidEndTour((tour: { title: string }) => {
-      console.log('Tour ended: ', tour.title);
-    });
+    // 将obs的output路径修改为当前路径.tour文件夹 ERROR: GetOutputSettings 没有找到output name相关定义
+    // ganVideoObject(codeTourApi);
   }
+}
+
+function ganVideoObject(
+  _context: vscode.ExtensionContext,
+  codeTourApi: {
+    onDidStartTour: (arg0: ([tour, stepNumber]: [{ title: string }, number]) => void) => void;
+    onDidEndTour: (arg0: (tour: { title: string }) => void) => void;
+  },
+  tourPath: string
+) {
+  // Use the API object as needed
+  codeTourApi.onDidStartTour(([tour, stepNumber]: [{ title: string }, number]) => {
+    const obs = OBSSubject.getSubject();
+    obs
+      .GetRecordStatus()
+      .pipe(map((res) => [res.responseData.outputDuration, res.responseData.outputTimecode]))
+      .subscribe({
+        next([_outputDuration, _outputTimecode]) {
+          const part = {
+            '@type': 'Clip',
+            name: `${stepNumber}: ${tour.title}`,
+            offset: `${_outputTimecode}`,
+            duration: _outputDuration,
+          };
+
+          _json_ld_template.hasPart.push(part);
+        },
+      });
+  });
+
+  codeTourApi.onDidEndTour((tour: { title: string }) => {
+    const part = {
+      '@type': 'Clip',
+      name: `结束语: ${tour.title}`,
+      offset: '-1',
+    };
+
+    _json_ld_template.hasPart.push(part);
+
+    writeFileSync(`${tourPath}/tourVideoObject.json`, JSON.stringify(_json_ld_template));
+
+    _json_ld_template.hasPart = [
+      {
+        '@type': 'Clip',
+        name: '前言',
+        offset: '0',
+      },
+    ];
+
+    const isRecording = _context.workspaceState.get('isRecording');
+    if (isRecording) vscode.commands.executeCommand(stopRecordCommandId);
+  });
 }
